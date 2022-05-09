2f5e5686446259b18992b1a2f91ee551a9cff0e3
gangpyono <shrkdvy123@gmail.com>
2022-05-10 01:23:11 +0900

    feat: filterKorean

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const { token } = require("./config.json");
@@ -6,6 +6,20 @@ const { token } = require("./config.json");
 client.once("ready", () => {
   // 봇 서버가 실행되면, 딱 한번만 실행하는 함수
   console.log(`실험봇이 활성화되었습니다`);
+});
+
+client.on("messageCreate", (msg) => {
+  const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+
+  if (korean.test(msg.content)) {
+    msg.delete();
+
+    msg.channel.send({
+      embeds: [
+        new MessageEmbed().setTitle("delete message").setFooter({ text: "Tlqkf dudgksxkfhgktpa" }),
+      ],
+    });
+  }
 });
 
 client.login(token); // 토큰에 내 봇 토큰 입력
